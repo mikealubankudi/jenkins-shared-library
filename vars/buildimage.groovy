02df0ba8dd92350f651a7e91a3816d8dbbe588e9
Michael Alubankudi <115126753+mikealubankudi@users.noreply.github.com>
2023-05-16 15:46:25 -0700

    Update buildimage.groovy

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t mikealubankudi/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push mikealubankudi/maven-app:2.0'
     }
